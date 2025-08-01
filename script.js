19aa91805de908baf32c4fed5d652111380d462f
Midokhala <monomo19888@gmail.com>
2025-08-01 15:11:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+function name() {
+    
+    console.log("meroo");
+    console.log("mohammmed");
+    console.log("zain ,  yasso  ,yehya");
+    
+    
+    
+}
+name();
+name();
+function sum (a, b, c){
+    console.log(a+b+c);
+    console.log(a*b*c);
+    console.log(a/b/c);
+console.log(a-c);  
+    
+}
+sum(1,2,3) 
+function sum2(a,b,c){
+    return a*b*c
+    return a/b/c
+}
+let result = sum2(1,9,55);
+console.log(result);
+
+
+let sum3 = (miramylove)  =>  {
+ return   (miramylove)
+}
+console.log(sum3);
+function printmyname(text){
+    console.log(text);
+    
+}
+printmyname("midoooo")
+let mynameis = (text) =>{
+    console.log(text);
+    
+}
+mynameis("zaain")
+const a=50;
+const b=100;
+console.log(a = b);
+
